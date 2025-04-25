f318ea2998f452a1a3edd34fc7c0a11e92dca48d
Vince (Neo) <halevince43@gmail.com>
2025-04-25 18:46:20 +0000

    HW9

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,16 @@
-#
+from class_a import Die
+
+def main():
+    die = Die()
+    while True:
+        input("Press Enter to roll the die...")
+        die.roll()
+        print(die)
+
+        again = input("Roll again? (y/n): ").lower()
+        if again != 'y':
+            print("Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,11 @@
-#
+import unittest
+from src.homework.j_classes.class_a import Die
+
+class TestDie(unittest.TestCase):
+    def test_roll_returns_value_between_1_and_6(self):
+        die = Die()
+        for _ in range(3):  # Test 3 rolls
+            die.roll()
+            val = die.get_rolled_value()
+            self.assertGreaterEqual(val, 1)
+            self.assertLessEqual(val, 6)
